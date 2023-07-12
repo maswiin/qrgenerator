582ed8c75f65df0bb1d107bb7b17100045fc9945
maswiin <138266206+maswiin@users.noreply.github.com>
2023-07-12 20:41:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // Data pengguna
 var users = [
-  { username: "", password: "$2b$10$7XV0p7aCk6ixLzjO.hoefuMfqo1pAWDCB.8xPfhCmCh0nqHwo4Haq" }, // Encrypted: tfcrdz1
-  { username: "yhnujm", password: "$2b$10$YcNECJ9yj27Nv.KI0FjBZ.Jd6X./TbNRZ.xQQckCzD.ZXoT7VeIBO" }, // Encrypted: plmnko2
-  { username: "xftyhn", password: "$2b$10$iW3CT6K1Xu7rDliqsuqfTuwFLCRoG3WsBX7vr06zxHa0LLnMCbw7S" }, // Encrypted: ijbvfr3
-  { username: "edcvfr", password: "$2b$10$Mf9f3ZmAwTYrNnF.GRf4q.du81h3tt7hiXoC4KgYwqIKJ8LmP8U56" }, // Encrypted: oknwsx4
-  { username: "ygvfr3", password: "$2b$10$UV.hgBgTTEsU6EE0tFJZOOI1Voj0fsuDBvRW/ww/XHnshQWFDd4mm" }, // Encrypted: yhbgu5
-  { username: "maswin", password: "$2b$10$L5L.vi9oG6B6GS89yzT8ru07tNlE3RYICtpuQidIE.K7aXkRhqJN2" },  // Encrypted: admin123
+  { username: "qazxsw", password: "tfcrdz1" },
+  { username: "yhnujm", password: "plmnko2" },
+  { username: "xftyhn", password: "ijbvfr3" },
+   { username: "edcvfr", password: "oknwsx4" },
+  { username: "ygvfr3", password: "yhbgu5" },
+  { username: "maswin", password: "admin123" },
 ];
 
 document.addEventListener("DOMContentLoaded", function() {
